cba6bfee67265bb60198954ff295d45558fc3686
Jorge Agullo <agullojorge@gmail.com>
2022-05-18 14:33:31 +0200

    [FEATURE] Added component & screen

--- app/src/main/java/com/practice/jettrivia/MainActivity.kt
+++ app/src/main/java/com/practice/jettrivia/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.practice.jettrivia
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,8 +8,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.practice.jettrivia.screens.QuestionsViewModel
+import com.practice.jettrivia.screens.TriviaHome
 import com.practice.jettrivia.ui.theme.JetTriviaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -31,21 +29,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
-    Questions(viewModel)
-}
-
-@Composable
-fun Questions(viewModel: QuestionsViewModel) {
-
-    val questions = viewModel.data.value.data?.toMutableList()
-
-    Log.d("SIZE", "Questions: ${questions?.size}")
-
-}
-
 
 @Preview(showBackground = true)
 @Composable
---
+++ app/src/main/java/com/practice/jettrivia/screens/TriviaHome.kt
@@ -0,0 +1,1 @@
+package com.practice.jettrivia.screensimport androidx.compose.runtime.Composableimport androidx.hilt.navigation.compose.hiltViewModelimport com.practice.jettrivia.component.Questions@Composablefun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {    Questions(viewModel)}
